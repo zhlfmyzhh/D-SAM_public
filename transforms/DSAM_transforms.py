be6cce59023300f5e12d57d13eb556b5ed656ec6
zhlfmyzhh <37364448+zhlfmyzhh@users.noreply.github.com>
2019-05-15 09:45:11 +0800

    Update DSAM_transforms.py

--- transforms/DSAM_transforms.py
+++ transforms/DSAM_transforms.py
@@ -3,11 +3,11 @@ from torchvision import transforms
 def get_transforms(output_size):
 
     train_transform = transforms.Compose([
-        transforms.RandomResizedCrop(output_size),
-        transforms.ColorJitter(.4, .4, .4, .4),
-        transforms.RandomHorizontalFlip(),
+        transforms.RandomResizedCrop(output_size),                  #随机大小，随机长宽比裁剪原始图片，最后将图片resize到设定好的size
+        transforms.ColorJitter(.4, .4, .4, .4),                     #修改修改亮度、对比度和饱和度
+        transforms.RandomHorizontalFlip(),                          #依据概率p对PIL图片进行水平翻转
         transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  #class torchvision.transforms.Normalize(mean, std)
         ])
 
     eval_transform = transforms.Compose([
